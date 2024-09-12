1edfd5f51bbaaad98d38d95d0616d244b593af04
Alan <alanpatlani65@gmail.com>
2024-09-12 11:30:50 -0600

    03 Operadores

---
+++ concepts/operadores.js
@@ -0,0 +1,37 @@
+// Operadores Matematicos
+// + y - 
+// * y / 
+
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+// Operadores String
+console.log("Hola" + " " + "Mundo");
+
+// Operadores de Asignacion
+let a = 1;
+a += 3;
+a -= 3;
+a *= 3;
+a /= 3;
+a %= 3;
+a **= 3;
+console.log(a);
+
+console.log(10 > 3);
+console.log(10 < 3);
+console.log(10 >= 3);
+console.log(10 <= 3);
+console.log(10 == 3);
+console.log(10 === 3);
+console.log(10 != 3);
+console.log(10 !== 3);
+
+// Oéradores Logicos
+console.log(true && true);
+console.log(true || true);
+console.log(!true);
