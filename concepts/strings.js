be64a1d8e66c8cba6bff8eb5769794df389a9ed1
Alan <alanpatlani65@gmail.com>
2024-09-12 11:56:13 -0600

    04 Strings

---
+++ concepts/strings.js
@@ -0,0 +1,13 @@
+// Strings en JavaScript
+
+let name = "Alan"
+let city = "Ixtenco"
+console.log("Hello, I'm " + name + " I live in " + city);
+
+console.log(message);
+message = "Hello, I'm " + name + " I live in" +city;
+console.log(message);
+
+console = 'Hello, I`m' + {name} + ' and I live in ' + {city};
+
+console.log(message);
