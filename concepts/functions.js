47728fef94fc119fc51933824e76193d1014908e
Alan <alanpatlani65@gmail.com>
2024-09-18 17:07:15 -0600

    Retos

---
+++ concepts/functions.js
@@ -0,0 +1,29 @@
+// function nameFunction(params){
+//     body 
+
+//     return
+// };
+
+// Arrow Function
+// const nameFunction = (params){
+//     body 
+
+//     return
+// };
+
+function sayHello(){
+    console.log('Hello');
+}
+
+sayHello();
+
+const sayHelloInSpanish = () => console.log('Hola');
+sayHelloInSpanish();
+
+function sayHelloTo1(name){
+    console.log('Hello ' + name);
+}
+
+const sayHello1 = name => console.log(`Hola ${name}`);
+sayHello1('Alan Patlani Flores');
+
---
+++ concepts/retos.js
@@ -0,0 +1,42 @@
+// Regalos Envueltos
+function wrapping(gifts) {
+    const envuelto = [];
+    for (let gift of gifts) {
+        let tapa = '*'.repeat(gift.length + 2);
+        let g = `${tapa}\n*${gift}*\n${tapa}`;
+        envuelto.push(g);
+    }
+    return envuelto;
+}
+
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+console.log(wrapped)
+
+// Cubo
+function createCube(size) {
+    const space = ' ';
+    const faceA = '/\\';
+    const backA = '_\\';
+    const faceB = '\\/';
+    const backB = '_/';
+    let cubeA = '';
+    for(let i = 0; i < size; i++){
+        let line = space.repeat(i);
+        line += faceA.repeat(size - i);
+        line += backA.repeat(size);
+        cubeA = line + '\n' + cubeA;
+    }
+    let cubeB = '';
+    for(let i = 0; i < size; i++){
+        let line = space.repeat(size - i - 1);
+        line += faceB.repeat(i + 1);
+        line += backB.repeat(size);
+        cubeB = line + '\n' + cubeB;
+    }
+    console.log(cubeA + cubeB);
+    return ''
+}
+
+const cube = createCube(3)
+console.log(cube)
